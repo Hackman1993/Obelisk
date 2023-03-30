75e1d54540c8a925198f8536b23292a85fe0ee18
Hackman.Lo <admin@yytec.net>
2023-03-30 17:58:50 +0800

    Init

--- test/controller/user_controller.cpp
+++ test/controller/user_controller.cpp
@@ -18,13 +18,7 @@ namespace obelisk {
     auto conn = DB::get_connection<rosetta::mysql_connection>("default");
     if(!conn) throw exception::http_exception("No Database Connection Available", 200);
     auto stmt = conn->prepared_statement("select * from t_user where username is not null limit 10");
-    //stmt.bind_param(1, username);
-    //stmt.bind_param(2, password);
     auto result = stmt.execute();
-    std::string password_encryption = result->get<rosetta::sql_string>(0, "created_at");
-    auto type = result->column_type("created_at");
-    type = result->column_type("password");
-
 
     return json_response(boost::polymorphic_pointer_cast<rosetta::sql_result>(result));
   }
